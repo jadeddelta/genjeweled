82eec0f38607956878b5867dfacdc27625566680
jadeddelta <101148768+jadeddelta@users.noreply.github.com>
2023-01-21 21:33:11 -0500

    Initial commit

---
+++ core/src/jadeddelta/genjwld/GenjeweledGame.java
@@ -0,0 +1,24 @@
+package jadeddelta.genjwld;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class GenjeweledGame extends ApplicationAdapter {
+	
+	@Override
+	public void create () {
+
+	}
+
+	@Override
+	public void render () {
+
+	}
+	
+	@Override
+	public void dispose () {
+
+	}
+}
